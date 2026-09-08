d828f718f8ed00f824beb3056028d4099ebe43e2
PapaGolf0422 <134192058+PapaGolf0422@users.noreply.github.com>
2026-09-08 15:47:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 //must be run as sudo!
 
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 apt-get install python3 python3-pip i2c-tools fonts-dejavu libjpeg-dev zlib1g-dev libfreetype6-dev liblcms1-dev libopenjp2-7 libtiff5 -y
-pip3 install rpi_ws281x adafruit-circuitpython-neopixel adafruit-circuitpython-ssd1306 pillow astral
+pip3 install rpi_ws281x adafruit-circuitpython-neopixel adafruit-circuitpython-ssd1306 pillow astral -y
 cp metar.py /home/pi
 cp pixelsoff.py /home/pi
 cp airports /home/pi
