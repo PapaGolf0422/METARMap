0a91b858a46017fcef855b2ade1396e27c669841
PapaGolf0422 <134192058+PapaGolf0422@users.noreply.github.com>
2026-09-08 17:10:48 -0500

    Create updategit.sh
    script to update from Git

---
+++ updategit.sh
@@ -0,0 +1,16 @@
+cd /home/pi/METARMap
+git pull
+\cp metar.py /home/pi
+\cp pixelsoff.py /home/pi
+\cp airports /home/pi
+\cp displayairports /home/pi
+\cp refresh.sh /home/pi
+\cp lightsoff.sh /home/pi
+\cp crontab /etc/crontab
+cd /home/pi
+chmod +x refresh.sh
+chmod +x lightsoff.sh
+chmod +r airports
+chmod +r displayairports
+chmod +r metar.py
+chmod +r pixelsoff.py
