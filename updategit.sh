a05fa95362d25532d6421a23a0aa65ce351c9a0c
PapaGolf0422 <134192058+PapaGolf0422@users.noreply.github.com>
2026-09-09 14:04:00 -0500

    Enhance updategit.sh with error handling and success message
    Updated the updategit.sh script to include error handling and added a success message after updating the repository.

--- updategit.sh
+++ updategit.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+set -e
+
 cd /home/pi/metarmap
 git pull
+
 \cp metar.py /home/pi
 \cp displaymetar.py /home/pi
 \cp pixelsoff.py /home/pi
@@ -8,6 +12,7 @@ git pull
 \cp refresh.sh /home/pi
 \cp lightsoff.sh /home/pi
 \cp crontab /etc/crontab
+
 cd /home/pi
 chmod +x refresh.sh
 chmod +x lightsoff.sh
@@ -16,3 +21,5 @@ chmod +r displayairports
 chmod +r metar.py
 chmod +r displaymetar.py
 chmod +r pixelsoff.py
+
+echo "Repository updated successfully."
