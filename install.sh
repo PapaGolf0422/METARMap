e89e6fb31fa89b1223975b4a17707ec1785b40e2
PapaGolf0422 <134192058+PapaGolf0422@users.noreply.github.com>
2026-09-09 11:58:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,5 @@ pip3 install astral
 \cp displayairports /home/pi
 \cp refresh.sh /home/pi
 \cp lightsoff.sh /home/pi
-\cp crontab /etc/crontab
 chmod +x updategit.sh
-cd /home/pi
-chmod +x refresh.sh
-chmod +x lightsoff.sh
-chmod +r airports
-chmod +r displayairports
-chmod +r metar.py
-chmod +r pixelsoff.py
+./updategit.sh
