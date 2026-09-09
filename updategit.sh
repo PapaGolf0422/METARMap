40c7f98f29359f1fe411e46817071ef31d1a0a81
PapaGolf0422 <134192058+PapaGolf0422@users.noreply.github.com>
2026-09-09 14:10:56 -0500

    Refactor updategit.sh for improved file handling
    Updated file permissions and modified git pull command.

--- updategit.sh
+++ updategit.sh
@@ -2,24 +2,20 @@
 set -e
 
 cd /home/pi/metarmap
-git pull
+/usr/bin/git pull
 
-\cp metar.py /home/pi
-\cp displaymetar.py /home/pi
-\cp pixelsoff.py /home/pi
-\cp airports /home/pi
-\cp displayairports /home/pi
-\cp refresh.sh /home/pi
-\cp lightsoff.sh /home/pi
-\cp crontab /etc/crontab
+\cp metar.py /home/pi/
+\cp displaymetar.py /home/pi/
+\cp pixelsoff.py /home/pi/
+\cp airports /home/pi/
+\cp displayairports /home/pi/
+\cp refresh.sh /home/pi/
+\cp lightsoff.sh /home/pi/
 
 cd /home/pi
-chmod +x refresh.sh
-chmod +x lightsoff.sh
-chmod +r airports
-chmod +r displayairports
-chmod +r metar.py
-chmod +r displaymetar.py
-chmod +r pixelsoff.py
+/bin/chmod +x updategit.sh
+/bin/chmod +x refresh.sh
+/bin/chmod +x lightsoff.sh
+/bin/chmod +r airports displayairports metar.py displaymetar.py pixelsoff.py
 
 echo "Repository updated successfully."
